1bef59e97fbc5ca0a536b815567c1b8b27906c75
MaciejSalamonski <macieksalamonski@gmail.com>
2020-08-06 17:20:39 +0200

    Class Map, added

---
+++ SHM/Map.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+
+#include "Island.hpp"
+
+class Map {
+    std::vector<Island> islands_;
+    Island* currentPosition_;
+
+public:
+    Map();
+    Island* getIsland(const Coordinates&);
+    Island* getCurrentPosition() const;
+};
