a071277d4b073e6e1aeea8d618808f95ac4d2947
MaciejSalamonski <macieksalamonski@gmail.com>
2020-08-17 23:12:44 +0200

    removing unnecessary files

--- pamsi/projekt1_algorytmy_sortowania/pliki_z_programem/mergesort.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifndef mergesort_h
-#define mergesort_h
-
-void merge(int* array, int begin, int mid, int end);
-void mergeSort(int* array, int begin, int end);
-
-#endif
--- pamsi/projekt1_algorytmy_sortowania/pliki_z_programem/quicksort.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#ifndef quicksort_h
-#define quicksort_h
-
-int pivot(int* array, int left, int right);
-void quickSort(int* array, int left, int right);
-
-#endif
