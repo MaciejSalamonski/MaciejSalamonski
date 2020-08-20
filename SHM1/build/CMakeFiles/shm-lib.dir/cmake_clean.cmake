d2d2f656a4d43f02236007d048e16b98650053d0
MaciejSalamonski <macieksalamonski@gmail.com>
2020-08-20 12:11:16 +0200

    Cmake added

---
+++ SHM1/build/CMakeFiles/shm-lib.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shm-lib.dir/ProgramFiles/src/Alcohol.cpp.o"
+  "CMakeFiles/shm-lib.dir/ProgramFiles/src/Cargo.cpp.o"
+  "CMakeFiles/shm-lib.dir/ProgramFiles/src/Fruit.cpp.o"
+  "CMakeFiles/shm-lib.dir/ProgramFiles/src/Item.cpp.o"
+  "libshm-lib.a"
+  "libshm-lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shm-lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- SHM1/build/CTestTestfile.cmake
+++ SHM1/build/CTestTestfile.cmake
@@ -4,6 +4,6 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(example_test "example")
-set_tests_properties(example_test PROPERTIES  _BACKTRACE_TRIPLES "/home/maciej/Documents/MyTasks/MaciejSalamonski/SHM1/CMakeLists.txt;52;add_test;/home/maciej/Documents/MyTasks/MaciejSalamonski/SHM1/CMakeLists.txt;0;")
+add_test(shm_test "/home/maciej/Documents/MyTasks/MaciejSalamonski/SHM1/build/shm-ut")
+set_tests_properties(shm_test PROPERTIES  _BACKTRACE_TRIPLES "/home/maciej/Documents/MyTasks/MaciejSalamonski/SHM1/CMakeLists.txt;63;add_test;/home/maciej/Documents/MyTasks/MaciejSalamonski/SHM1/CMakeLists.txt;0;")
 subdirs("googletest-build")
