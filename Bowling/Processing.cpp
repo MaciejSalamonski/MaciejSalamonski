c1f3a70ecb5ae170ba7587b11642ca24fa26b1ac
MaciejSalamonski <macieksalamonski@gmail.com>
2020-09-18 13:43:33 +0200

    processing update

--- Bowling/Processing.cpp
+++ Bowling/Processing.cpp
@@ -1,19 +1,17 @@
-#include <ctype.h>
 #include <algorithm>
 #include <iostream>
-#include <map>
 #include <string>
 #include <vector>
 
-std::string prepareStringWithPlayerResult(std::string& playerResults) {
+std::string stringPreparing(std::string& playerResults) {
     const char addToEndOfString = '|';
     playerResults.push_back(addToEndOfString);
 
     return playerResults;
 }
 
-std::vector<std::string> foo(std::string& preparedStringWithPlayerResults) {
-    std::string delimiter = "|";
+std::vector<std::string> stringSplitting(std::string& preparedStringWithPlayerResults) {
+    const std::string delimiter = "|";
     size_t position = 0;
     std::string token;
     std::vector<std::string> vectorWithResultsForEachLine{};
@@ -26,40 +24,44 @@ std::vector<std::string> foo(std::string& preparedStringWithPlayerResults) {
     return vectorWithResultsForEachLine;
 }
 
-constexpr uint8_t STRIKE = 10;
-constexpr uint8_t ZERO_POINTS = 0;
+constexpr uint8_t strike = 10;
+constexpr uint8_t zeroPoints = 0;
+constexpr char isStrike = 'X';
+constexpr char isMiss = '-';
+constexpr char isSpare = '/';
+constexpr char convertCharToInt = '0';
 
-std::vector<int> bar(std::vector<std::string>& vectorWithResultsForEachLine) {
-    std::vector<int> vectorWithResults;
+std::vector<int> stringParsing(std::vector<std::string>& vectorWithResultsForEachLine) {
+    std::vector<int> vectorWithResults{};
 
     std::find_if(vectorWithResultsForEachLine.cbegin(), vectorWithResultsForEachLine.cend(),
                  [&vectorWithResults](std::string bowlingLineString) {
                      std::find_if(bowlingLineString.cbegin(), bowlingLineString.cend(),
-                                  [&vectorWithResults, &bowlingLineString](char charackter) {
-                                      if (charackter == 'X') {
-                                          vectorWithResults.push_back(STRIKE);
-                                          vectorWithResults.push_back(ZERO_POINTS);
+                                  [&vectorWithResults, &bowlingLineString](const char charackter) {
+                                      if (charackter == isStrike) {
+                                          vectorWithResults.push_back(strike);
+                                          vectorWithResults.push_back(zeroPoints);
                                       }
 
-                                      if (charackter == '-') {
-                                          vectorWithResults.push_back(ZERO_POINTS);
+                                      if (charackter == isMiss) {
+                                          vectorWithResults.push_back(zeroPoints);
                                       }
 
                                       if (isdigit(charackter)) {
-                                          if (bowlingLineString[1] == '/') {
-                                              uint8_t firstThrow = *bowlingLineString.begin() - '0';
-                                              uint8_t secondThrow = STRIKE - firstThrow;
+                                          if (bowlingLineString[1] == isSpare) {
+                                              uint8_t firstThrow = *bowlingLineString.begin() - convertCharToInt;
+                                              uint8_t secondThrow = strike - firstThrow;
                                               vectorWithResults.push_back(firstThrow);
                                               vectorWithResults.push_back(secondThrow);
                                               return true;
                                           }
-                                          uint8_t charackterToInteger = charackter - '0';
+                                          uint8_t charackterToInteger = charackter - convertCharToInt;
                                           vectorWithResults.push_back(charackterToInteger);
                                       }
 
                                       if (isspace(charackter)) {
-                                          vectorWithResults.push_back(ZERO_POINTS);
-                                          vectorWithResults.push_back(ZERO_POINTS);
+                                          vectorWithResults.push_back(zeroPoints);
+                                          vectorWithResults.push_back(zeroPoints);
                                           return true;
                                       }
 
@@ -72,22 +74,22 @@ std::vector<int> bar(std::vector<std::string>& vectorWithResultsForEachLine) {
     return vectorWithResults;
 }
 
-void splitAndPrepareString(std::string& processingString, std::string& nickName, std::vector<int>& playerResult) {
+void stringProcessing(std::string& processingString, std::string& nickName, std::vector<int>& playerResult) {
     auto delimiterPosition = processingString.find_first_of(':');
     std::string playerResults = processingString.substr(++delimiterPosition),
                 playerName = processingString.substr(0, delimiterPosition);
     nickName = playerName;
 
-    std::string preparedStringWithPlayerResults = prepareStringWithPlayerResult(playerResults);
-    std::vector<std::string> vectorWithResultsForEachLine = foo(preparedStringWithPlayerResults);
-    playerResult = bar(vectorWithResultsForEachLine);
+    std::string preparedStringWithPlayerResults = stringPreparing(playerResults);
+    std::vector<std::string> vectorWithResultsForEachLine = stringSplitting(preparedStringWithPlayerResults);
+    playerResult = stringParsing(vectorWithResultsForEachLine);
 }
 
 int main() {
-    std::string processingString = "Michael:X|7/|9-|X|-8|8/|-6|X|X|X| |81";
+    std::string processingString = "Michael:X|7/|9-|X|-8|8/|-6|X|X|X||81";
     std::string nickName;
     std::vector<int> playerResult;
-    splitAndPrepareString(processingString, nickName, playerResult);
+    stringProcessing(processingString, nickName, playerResult);
 
     std::cout << nickName << '\n';
     for (const auto& el : playerResult) {
@@ -97,6 +99,3 @@ int main() {
 
     return 0;
 }
-
-//Michael:X|7/|9-|X|-8|8/|-6|X|X|X||81
-//wektor [10,0,7,3,9,0,10,0,0,8,8,2,0,6,10,0,10,0,10,0,0,0,8,1]
